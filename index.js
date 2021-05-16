a6b40a6db066194fe5c51f6eac4229d8e33e1578
Virej Dasani <dasanivirej@gmail.com>
2021-05-16 20:00:45 +0530

    Bug fixes

--- index.js
+++ index.js
@@ -2,36 +2,16 @@ const iPhoneX = document.getElementById('iPhoneX')
 
 // When webview is loaded
 iPhoneX.addEventListener('dom-ready', () => {
-    // https://www.electronjs.org/docs/api/webview-tag
-
     // This is to hide scrollbars in the devices
     iPhoneX.insertCSS(`
         ::-webkit-scrollbar {
             display: none;
         }
     `)
-
     iPhoneX.setZoomFactor(0.6)
-
-    // if (webview.isLoading) {
-    // alert('LOADING')
-    // }
-
-    // webview.openDevTools()
-
-    // To go back
-    // if (webview.canGoBack) {
-    //     webview.goBack()
-    // }
-
-    // To go forward
-    // if (webview.canGoForward) {
-    //     webview.goForward()
-    // }
 })
 
 const S21Ultra = document.getElementById('S21Ultra')
-
 
 S21Ultra.addEventListener('dom-ready', () => {
     // This is to hide scrollbars in the devices
@@ -40,6 +20,35 @@ S21Ultra.addEventListener('dom-ready', () => {
             display: none;
         }
     `)
-
     S21Ultra.setZoomFactor(0.6)
 })
+
+// function deviceSettings(deviceName, zoomFactor, cssToInsert) {
+//     const deviceName = document.getElementById(deviceName)
+//     S21Ultra.addEventListener('dom-ready', () => {
+//         // This is to hide scrollbars in the devices
+//         S21Ultra.insertCSS(`
+//             ::-webkit-scrollbar {
+//                 display: none;
+//             }
+//         `)
+//         S21Ultra.setZoomFactor(0.6)
+//     })
+// }
+
+// Methods from https://www.electronjs.org/docs/api/webview-tag
+// if (webview.isLoading) {
+// alert('LOADING')
+// }
+
+// webview.openDevTools()
+
+// To go back
+// if (webview.canGoBack) {
+//     webview.goBack()
+// }
+
+// To go forward
+// if (webview.canGoForward) {
+//     webview.goForward()
+// }
