cc586f509613d3f522ac54f43b15526a3420d746
Virej Dasani <dasanivirej@gmail.com>
2021-05-16 18:31:35 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const webview = document.querySelector('webview')
+
+// When webview is loaded
+webview.addEventListener('dom-ready', () => {
+// https://www.electronjs.org/docs/api/webview-tag
+
+    // if (webview.isLoading) {
+        // alert('LOADING')
+    // }
+
+    // webview.openDevTools()
+
+    // To go back
+    // if (webview.canGoBack) {
+    //     webview.goBack()
+    // }
+
+    // To go forward
+    // if (webview.canGoForward) {
+    //     webview.goForward()
+    // }
+
+    // webview.setZoomFactor(0.5)
+})
---
+++ main.js
@@ -0,0 +1,40 @@
+// Modules to control application life and create native browser window
+const {
+    app,
+    BrowserWindow
+} = require('electron')
+
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            webviewTag: true
+        }
+    })
+
+    // and load the index.html of the app.
+    win.loadFile('index.html')
+
+    // Open the DevTools.
+    // win.webContents.openDevTools()
+}
+
+// When app is ready
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
