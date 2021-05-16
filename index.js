e79dfbe420265673de8a7c69f921e6aa2085ea10
Virej Dasani <dasanivirej@gmail.com>
2021-05-16 20:04:03 +0530

    Made code more efficient

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
-const iPhoneX = document.getElementById('iPhoneX')
+// const iPhoneX = document.getElementById('iPhoneX')
 
-// When webview is loaded
-iPhoneX.addEventListener('dom-ready', () => {
-    // This is to hide scrollbars in the devices
-    iPhoneX.insertCSS(`
-        ::-webkit-scrollbar {
-            display: none;
-        }
-    `)
-    iPhoneX.setZoomFactor(0.6)
-})
+// // When webview is loaded
+// iPhoneX.addEventListener('dom-ready', () => {
+//     // This is to hide scrollbars in the devices
+//     iPhoneX.insertCSS(`
+//         ::-webkit-scrollbar {
+//             display: none;
+//         }
+//     `)
+//     iPhoneX.setZoomFactor(0.6)
+// })
 
-const S21Ultra = document.getElementById('S21Ultra')
+// const S21Ultra = document.getElementById('S21Ultra')
 
-S21Ultra.addEventListener('dom-ready', () => {
-    // This is to hide scrollbars in the devices
-    S21Ultra.insertCSS(`
-        ::-webkit-scrollbar {
-            display: none;
-        }
-    `)
-    S21Ultra.setZoomFactor(0.6)
-})
+// S21Ultra.addEventListener('dom-ready', () => {
+//     // This is to hide scrollbars in the devices
+//     S21Ultra.insertCSS(`
+//         ::-webkit-scrollbar {
+//             display: none;
+//         }
+//     `)
+//     S21Ultra.setZoomFactor(0.6)
+// })
 
-// function deviceSettings(deviceName, zoomFactor, cssToInsert) {
-//     const deviceName = document.getElementById(deviceName)
-//     S21Ultra.addEventListener('dom-ready', () => {
-//         // This is to hide scrollbars in the devices
-//         S21Ultra.insertCSS(`
-//             ::-webkit-scrollbar {
-//                 display: none;
-//             }
-//         `)
-//         S21Ultra.setZoomFactor(0.6)
-//     })
-// }
+function calibrateDevice(deviceName) {
+    var deviceName = document.getElementById(deviceName)
+
+    deviceName.addEventListener('dom-ready', () => {
+        // This is to hide scrollbars in the devices
+        deviceName.insertCSS(`
+            ::-webkit-scrollbar {
+                display: none;
+            }
+        `)
+        deviceName.setZoomFactor(0.6)
+    })
+}
+
+calibrateDevice("iPhoneX")
+calibrateDevice("S21Ultra")
 
 // Methods from https://www.electronjs.org/docs/api/webview-tag
 // if (webview.isLoading) {
