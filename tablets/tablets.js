385adb5b6eab17e7dadc80dd9b74fb9d151d0531
Virej Dasani <dasanivirej@gmail.com>
2021-05-21 20:03:34 +0530

    Added landscape for tablet

--- tablets/tablets.js
+++ tablets/tablets.js
@@ -3,7 +3,7 @@ var website = document.getElementById("urlInput").value
 let goButton = document.getElementById("goButton")
 
 // All devices go in the devices array
-let devices = ["#iPadPro", "#iPadMini"]
+let devices = ["#iPadPro", "#iPadMini", "#iPadMiniLandscape"]
 
 website = `https://virejdasani.github.io/Geniemoji/`
 
